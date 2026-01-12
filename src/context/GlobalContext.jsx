39490cf80b13d08b59d195407e50d5b660bc14c3
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2026-01-12 19:47:48 +0100

    Add GlobalContext logic for 2 card comparator and for favorites.

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,7 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  const [favorites, setFavorites] = useState([]); // array vuoto per salvare i preferiti
+  const [comparator, setComparator] = useState([]); //array vuoto per il comparatore di carte
+  //aggiunge o rimuove l'elemento dai preferiti
+  const handleFavorite = (card) => {
+    setFavorites((prev) =>
+      prev.find((c) => c.id === card.id)
+        ? prev.filter((c) => c.id !== card.id)
+        : [...prev, card]
+    );
+  };
+
+  //Aggiunge/rimuove le carte dal comparatore (atm max 2 carte)
+  const handleComparator = (card) => {
+    setComparator((prev) => {
+      const exists = prev.find((c) => c.id === card.id);
+      if (exists) return prev.filter((c) => c.id !== card.id);
+      if (prev.length < 2) return [...prev, card];
+      return prev; // non mette pià di due carte
+    });
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{ favorites, handleFavorite, comparator, handleComparator }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
